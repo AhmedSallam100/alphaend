6ecec6e33fbbda48759c9607406d6e633f3742c4
Ahmed Sallam <ahmedsallam121212u@gmail.com>
2023-08-25 20:31:27 +0300

    first commit

---
+++ client/src/Redux/codersWeb.js
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const codersWebApi = createApi({
+  reducerPath: "codersWebApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337/api/" }),
+  endpoints: (builder) => ({
+    getcodersWebByName: builder.query({
+      query: (name) => `coderswebs${name}`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetcodersWebByNameQuery } = codersWebApi;
---
+++ client/src/Redux/colorsWeb.js
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const colorsWebApi = createApi({
+  reducerPath: "colorsWebApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337/api/" }),
+  endpoints: (builder) => ({
+    getcolorsWebByName: builder.query({
+      query: (name) => `colorswebs${name}`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetcolorsWebByNameQuery } = colorsWebApi;
